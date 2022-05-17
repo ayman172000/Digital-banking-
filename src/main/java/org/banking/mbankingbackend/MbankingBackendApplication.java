ec1133b96752edbd81173e9d5b45cd771b966ef7
ayman rah <rahmani.ayman17@gmail.com>
2022-05-17 21:21:03 +0100

    fix CROS bug in controller @CroosOrigin(*)

--- src/main/java/org/banking/mbankingbackend/MbankingBackendApplication.java
+++ src/main/java/org/banking/mbankingbackend/MbankingBackendApplication.java
@@ -1,13 +1,39 @@
 package org.banking.mbankingbackend;
 
+import org.banking.mbankingbackend.dtos.CurrentBankAccountDTO;
+import org.banking.mbankingbackend.dtos.CustomerDTO;
+import org.banking.mbankingbackend.entities.Customer;
+import org.banking.mbankingbackend.services.BankAccountServiceImpl;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class MbankingBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MbankingBackendApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(BankAccountServiceImpl bankAccountService)
+    {
+        return args -> {
+            CustomerDTO customerDTO=new CustomerDTO();
+            customerDTO.setId("1");
+            customerDTO.setMail("ayman@gmail.com");
+            customerDTO.setNom("ayman");
+            bankAccountService.saveCustomer(customerDTO);
+            CustomerDTO customerDTO1=new CustomerDTO();
+            customerDTO1.setId("2");
+            customerDTO1.setMail("mouad@gmail.com");
+            customerDTO1.setNom("mouad");
+            bankAccountService.saveCustomer(customerDTO1);
+            //bankAccountService.saveCurrentBankAccount(1000l,10,)
+        };
     }
 
 }
--- src/main/java/org/banking/mbankingbackend/web/BankAccountRestController.java
+++ src/main/java/org/banking/mbankingbackend/web/BankAccountRestController.java
@@ -2,21 +2,19 @@ package org.banking.mbankingbackend.web;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.banking.mbankingbackend.dtos.AccountHistoryDTO;
-import org.banking.mbankingbackend.dtos.AccountOperationDTO;
-import org.banking.mbankingbackend.dtos.BankAccountDTO;
+import org.banking.mbankingbackend.dtos.*;
+import org.banking.mbankingbackend.entities.CurrentAccount;
 import org.banking.mbankingbackend.services.BankAccountService;
 import org.banking.mbankingbackend.services.exception.BankAccountNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.banking.mbankingbackend.services.exception.CustomerNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 public class BankAccountRestController {
     BankAccountService bankAccountService;
 
@@ -42,5 +40,17 @@ public class BankAccountRestController {
                                                @RequestParam(name = "page",defaultValue = "0") int page,
                                                @RequestParam(name = "page",defaultValue = "5")int size) throws BankAccountNotFoundException {
        return bankAccountService.getAccountHistory(accountId,page,size);
+    }
+
+    /*@PostMapping("/accounts")
+    public BankAccountDTO saveCurrentAccount(@RequestBody double initialBalance,@RequestBody double decouvert,@RequestBody String customerId ) throws CustomerNotFoundException {
+        //saveCurrentBankAccount(double initialBalance, double decouvert, String customerId)
+        return bankAccountService.saveCurrentBankAccount(initialBalance,decouvert,customerId);
+
+    }*/
+    @PostMapping("/accounts")
+    public BankAccountDTO saveSavingAccount(@RequestBody double initialBalance,@RequestBody double interestRate,@RequestBody String customerId) throws CustomerNotFoundException {
+        //saveSavingBankAccount(double initialBalance, double interestRate, String customerId)
+        return bankAccountService.saveSavingBankAccount(initialBalance,interestRate,customerId);
     }
 }
